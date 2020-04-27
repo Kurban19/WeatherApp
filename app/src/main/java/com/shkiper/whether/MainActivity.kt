e32fc88c27817be91dbef9d34b1f8b290f593913
Shkiper01 <baga5828391@gmail.com>
2020-04-27 20:58:25 +0300

    bind views with json

--- app/src/main/java/com/shkiper/whether/MainActivity.kt
+++ app/src/main/java/com/shkiper/whether/MainActivity.kt
@@ -7,6 +7,11 @@ import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.shkiper.whether.common.Common
+import kotlinx.android.synthetic.main.activity1_main.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.errorText
+import kotlinx.android.synthetic.main.activity_main.loader
 import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
@@ -19,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity1_main)
+        setContentView(R.layout.activity_main)
 
         weatherTask().execute()
 
@@ -29,9 +34,9 @@ class MainActivity : AppCompatActivity() {
         override fun onPreExecute() {
             super.onPreExecute()
             /* Showing the ProgressBar, Making the main design GONE */
-            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
-            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
-            findViewById<TextView>(R.id.errorText).visibility = View.GONE
+            loader.visibility = View.VISIBLE
+            //mainContainer.visibility = View.GONE
+            errorText.visibility = View.GONE
         }
 
         override fun doInBackground(vararg params: String?): String? {
@@ -50,45 +55,63 @@ class MainActivity : AppCompatActivity() {
             super.onPostExecute(result)
             try {
                 /* Extracting JSON returns from the API */
+                //mainContainer.visibility = View.VISIBLE
                 val jsonObj = JSONObject(result)
                 val main = jsonObj.getJSONObject("main")
-                val sys = jsonObj.getJSONObject("sys")
-                val wind = jsonObj.getJSONObject("wind")
-                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-
-                val updatedAt:Long = jsonObj.getLong("dt")
-                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
                 val temp = main.getString("temp")+"°C"
                 val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
                 val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
-                val pressure = main.getString("pressure")
-                val humidity = main.getString("humidity")
-
-                val sunrise:Long = sys.getLong("sunrise")
-                val sunset:Long = sys.getLong("sunset")
-                val windSpeed = wind.getString("speed")
-                val weatherDescription = weather.getString("description")
-
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val sys = jsonObj.getJSONObject("sys")
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
 
-                /* Populating extracted data into our views */
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
-                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
-                findViewById<TextView>(R.id.temp).text = temp
-                findViewById<TextView>(R.id.temp_min).text = tempMin
-                findViewById<TextView>(R.id.temp_max).text = tempMax
-                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
-                findViewById<TextView>(R.id.wind).text = windSpeed
-                findViewById<TextView>(R.id.pressure).text = pressure
-                findViewById<TextView>(R.id.humidity).text = humidity
+                curr_temp.text = temp
+                txt_city.text = address
+                txt_updated_at.text = updatedAtText
 
-                /* Views populated, Hiding the loader, Showing the main design */
-                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
+
+                loader.visibility = View.GONE
+
+
+
+
+//                val wind = jsonObj.getJSONObject("wind")
+//                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+//
+//                val updatedAt:Long = jsonObj.getLong("dt")
+//                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+//                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
+//                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
+//                val pressure = main.getString("pressure")
+//                val humidity = main.getString("humidity")
+//
+//                val sunrise:Long = sys.getLong("sunrise")
+//                val sunset:Long = sys.getLong("sunset")
+//                val windSpeed = wind.getString("speed")
+//                val weatherDescription = weather.getString("description")
+//
+//                val address = jsonObj.getString("name")+", "+sys.getString("country")
+//
+//                /* Populating extracted data into our views */
+//                findViewById<TextView>(R.id.address).text = address
+//                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
+//                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
+//                findViewById<TextView>(R.id.temp).text = temp
+//                findViewById<TextView>(R.id.temp_min).text = tempMin
+//                findViewById<TextView>(R.id.temp_max).text = tempMax
+//                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+//                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+//                findViewById<TextView>(R.id.wind).text = windSpeed
+//                findViewById<TextView>(R.id.pressure).text = pressure
+//                findViewById<TextView>(R.id.humidity).text = humidity
+//
+//                /* Views populated, Hiding the loader, Showing the main design */
+//                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+//                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
 
             } catch (e: Exception) {
+                txt_city.text = e.message.toString()
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
                 findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
             }
