ffd2fc44f1ac10e39d83f3e5880910e230f1aeb0
Shkiper01 <baga5828391@gmail.com>
2020-04-28 13:46:53 +0300

    add geoLocation requests

--- app/src/main/java/com/shkiper/whether/MainActivity.kt
+++ app/src/main/java/com/shkiper/whether/MainActivity.kt
@@ -1,32 +1,46 @@
 package com.shkiper.whether
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 import com.shkiper.whether.common.Common
-import kotlinx.android.synthetic.main.activity1_main.*
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.errorText
-import kotlinx.android.synthetic.main.activity_main.loader
 import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
 
-    val CITY: String = "dhaka,bd"
-    val API: String = "a6f096e9c7045fffc90db97209788939"
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+    val PERMISSION_ID = 42
+    //val CITY: String = "dhaka,bd"
+    //val API: String = "a6f096e9c7045fffc90db97209788939"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        weatherTask().execute()
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLastLocation()
+
+        //weatherTask().execute()
 
     }
 
@@ -40,82 +54,129 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun doInBackground(vararg params: String?): String? {
-            var response:String?
-            try{
-                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
-                    Charsets.UTF_8
-                )
+            return try{
+                              //"https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API"
+                    URL("https://api.openweathermap.org/data/2.5/weather?lat=${Common.current_location.latitude}&lon=${Common.current_location.longitude}&units=metric&appid=${Common.APP_ID}").readText(
+                        Charsets.UTF_8
+                    )
             }catch (e: Exception){
-                response = null
+                null
             }
-            return response
         }
 
+        @SuppressLint("SetTextI18n")
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             try {
                 /* Extracting JSON returns from the API */
-                //mainContainer.visibility = View.VISIBLE
                 val jsonObj = JSONObject(result)
                 val main = jsonObj.getJSONObject("main")
-                val temp = main.getString("temp")+"°C"
-                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
-                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
+                val temp = main.getString("temp")
+                //val weather = jsonObj.getJSONObject("weather")
                 val updatedAt:Long = jsonObj.getLong("dt")
-                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val updatedAtText = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.ENGLISH).format(Date(updatedAt*1000))
                 val sys = jsonObj.getJSONObject("sys")
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
 
-                curr_temp.text = temp
+
+                val index: Int = temp.indexOf(".") //Removing all the useless character in temp. Example 12.32342 -> 12
+                curr_temp.text = temp.substring(0, index) + "°C"
                 txt_city.text = address
                 txt_updated_at.text = updatedAtText
+                //txt_description.text = description
 
 
                 loader.visibility = View.GONE
 
 
 
-
-//                val wind = jsonObj.getJSONObject("wind")
-//                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-//
-//                val updatedAt:Long = jsonObj.getLong("dt")
-//                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
-//                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
-//                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
-//                val pressure = main.getString("pressure")
-//                val humidity = main.getString("humidity")
-//
-//                val sunrise:Long = sys.getLong("sunrise")
-//                val sunset:Long = sys.getLong("sunset")
-//                val windSpeed = wind.getString("speed")
-//                val weatherDescription = weather.getString("description")
-//
-//                val address = jsonObj.getString("name")+", "+sys.getString("country")
-//
-//                /* Populating extracted data into our views */
-//                findViewById<TextView>(R.id.address).text = address
-//                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
-//                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
-//                findViewById<TextView>(R.id.temp).text = temp
-//                findViewById<TextView>(R.id.temp_min).text = tempMin
-//                findViewById<TextView>(R.id.temp_max).text = tempMax
-//                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-//                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
-//                findViewById<TextView>(R.id.wind).text = windSpeed
-//                findViewById<TextView>(R.id.pressure).text = pressure
-//                findViewById<TextView>(R.id.humidity).text = humidity
-//
-//                /* Views populated, Hiding the loader, Showing the main design */
-//                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-//                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
-
             } catch (e: Exception) {
-                txt_city.text = e.message.toString()
-                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+                println(e.stackTrace)
+                //txt_city.text = e.message
+                loader.visibility = View.GONE
+                errorText.visibility = View.VISIBLE
             }
 
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        Common.current_location.latitude = location.latitude
+                        Common.current_location.longitude = location.longitude
+                        weatherTask().execute()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+        }
+    }
+
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Granted. Start getting the location information
+//
+            }
         }
     }
 }
--- app/src/main/java/com/shkiper/whether/common/Common.java
+++ app/src/main/java/com/shkiper/whether/common/Common.java
@@ -9,7 +9,7 @@ public class Common {
 
     public static final String APP_ID = "a6f096e9c7045fffc90db97209788939";
 
-    public static Location current_location = null;
+    public static Location current_location = new Location("dhaka,bd");
 
 
 }
